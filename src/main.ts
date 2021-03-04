98cfb81f5efed21a46bbe2b62b939d3ff653bb1b
cladams0203 <cladams0203@gmail.com>
2021-03-04 18:37:17 -0500

    added swagger for docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger Options
+  const options = new DocumentBuilder()
+    .setTitle('Dragon Ball Anime API')
+    .setDescription('API for the Dragon Ball TV Series')
+    .setVersion('1.0.0')
+    .build();
+  //build swagger documents
+  const docs = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, docs);
+
+  const port = process.env.PORT || 5000;
+
+  await app.listen(port, () => console.log(`App is running on port ${port}`));
 }
 bootstrap();
